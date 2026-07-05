efef33e1b99dc1c776966c9f8c5decd32bed07f4
commenti <commenting761@gmail.com>
2026-07-05 14:39:24 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ android {
 }
 
 dependencies {
+    implementation "androidx.datastore:datastore-preferences:1.1.1"
+    implementation "androidx.security:security-crypto:1.1.0-alpha06"
     implementation("androidx.work:work-runtime-ktx:2.10.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
